70c1081e5df113c34097722e7892893ac7748cbb
5ync <romain1121@naver.com>
2026-03-31 15:58:47 +0900

    feat: 변경된 스키마 구조에 따른 db_loader 개편

--- common/db_loader.py
+++ common/db_loader.py
@@ -49,23 +49,30 @@ def _execute_load(cursor, data, mode="INSERT"):
                     cursor.executemany(sql_attachment, attachment_values)
 
         elif mode == "UPDATE":
-            # URL을 기준으로 article_id 찾기
-            find_id_sql = "SELECT article_id FROM school_article_vendors WHERE original_url = %s LIMIT 1"
-            cursor.execute(find_id_sql, (article['original_url'],))
-            res = cursor.fetchone()
+            # [1] 다중 URL 중 하나라도 매칭되는 article_id 찾기
+            vendor_urls = article.get('vendor_urls', [])
+            article_id = None
+            
+            if vendor_urls:
+                format_strings = ','.join(['%s'] * len(vendor_urls))
+                find_id_sql = f"SELECT article_id FROM school_article_vendors WHERE original_url IN ({format_strings}) LIMIT 1"
+                cursor.execute(find_id_sql, tuple(vendor_urls))
+                res = cursor.fetchone()
+                if res:
+                    article_id = res['article_id']
 
-            if res:
-                article_id = res['article_id']
-
-                # [1] school_articles 업데이트
+            if article_id:
+                # [2] school_articles 업데이트
                 sql_update_art = """
                                  UPDATE school_articles
-                                 SET title       = %s, \
-                                     content     = %s, \
-                                     updated_at  = %s, \
-                                     category_id = %s,
-                                     start_date  = %s, \
-                                     due_date    = %s
+                                 SET title        = %s, \
+                                     content      = %s, \
+                                     updated_at   = %s, \
+                                     category_id  = %s, \
+                                     start_date   = %s, \
+                                     due_date     = %s, \
+                                     is_published = 0, \
+                                     admin_status = 'INSPECTED_YET'
                                  WHERE article_id = %s \
                                  """
                 cursor.execute(sql_update_art, (
@@ -74,7 +81,7 @@ def _execute_load(cursor, data, mode="INSERT"):
                     article.get('start_date'), article.get('due_date'), article_id
                 ))
 
-                # [2] 맵핑 정보 갱신
+                # [3] 맵핑 정보 갱신
                 vendor_ids = article.get('vendor_ids', [])
                 vendor_urls = article.get('vendor_urls', [])
                 for v_id, url in zip(vendor_ids, vendor_urls):
@@ -84,7 +91,7 @@ def _execute_load(cursor, data, mode="INSERT"):
                         sql_ins_map = "INSERT INTO school_article_vendors (article_id, vendor_id, original_url) VALUES (%s, %s, %s)"
                         cursor.execute(sql_ins_map, (article_id, int(v_id), url))
 
-                # [3] 첨부파일 갱신
+                # [4] 첨부파일 갱신
                 attachments = article.get('attachments', [])
                 for att in attachments:
                     url = att.get('attachment_url')
